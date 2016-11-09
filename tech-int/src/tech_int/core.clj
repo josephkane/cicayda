d2b6e354e3a08406becd6a9e0515e523991c29e5
Joseph Kane <josephckane@gmail.com>
2016-11-09 17:47:41 -0600

    mvp leap year clojure

--- tech-int/src/tech_int/core.clj
+++ tech-int/src/tech_int/core.clj
@@ -3,57 +3,33 @@
 
 ; LEAP YEAR
 
-; (and (zero? (mod year 400)) (and (zero? (mod year 4)) (not= (zero? (mod year 100)))))
-
-; (zero? (mod year 400))
-
-; (and (zero? (mod year 4)) (not= (zero? (mod year 100))))
-
-
-
 (defn checkIfLeapYear
     [year]
-    (println year)
     (if (or (zero? (mod year 400)) (and (zero? (mod year 4)) (not= 0 (mod year 100))))
         true
         false
     )
 )
 
-; (defn -leapYear []
-;     (println "Enter a year: ")
-;     (def inputYear (Integer. (read-line)))
-;     (loop [leapYearVector []
-;            year inputYear]
-;         (when (< (count leapYearVector) 20)
-;             (if (checkIfLeapYear year)
-;                 (do
-;                     (conj leapYearVector year)
-;                     (println year)
-;                 ))
-
-;             (recur leapYearVector (inc year))
-;         )
-;     )
-; )
-
-(defn leapYear [year]
-    (def i (atom 1))
-    (while (<= @i 20)
-        (do
+(defn -leapYear []
+    (println "Enter a year: ")
+    (def inputYear (Integer. (read-line)))
+    (println "--")
+    (def i (atom 0))
+    (loop [leapYearVector []
+           year inputYear]
+        (when (< @i 20)
             (if (checkIfLeapYear year)
                 (do
+                    (conj leapYearVector year)
+                    (swap! i inc)
                     (println year)
-                    (inc year)
-                    (swap! @i inc)
-                )
-                (inc year)
-            )
+                ))
+
+            (recur leapYearVector (inc year))
         )
     )
 )
 
 
-(println "Enter a year: ")
-(def inputYear (Integer. (read-line)))
-(leapYear inputYear)
+
