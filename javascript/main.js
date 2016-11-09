cc5443834d0e6bd6309b89f01c7d0bbbe98c9f30
Joseph Kane <josephckane@gmail.com>
2016-11-09 09:30:06 -0600

    mvp for javascript sq phrase

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,18 @@
+/****************
+LEAP YEARS
+****************/
 
 const leapYearButton = document.getElementById("leapYearButton")
 const showMeLeapYearsButton = document.getElementById("showMeLeapYears")
-const sqPhraseButton = document.getElementById("sqPhraseButton")
-const pigLatinButton = document.getElementById("pigLatinButton")
-
 const leapYearDiv = document.getElementById("leapYear")
-const sqPhraseDiv = document.getElementById("sqPhrase")
-const pigLatinDiv = document.getElementById("pigLatin")
-
+const nextTwentyLeapYears = document.getElementById("nextTwentyLeapYears")
 leapYearButton.addEventListener("click", () => {leapYearDiv.style.display = "block"})
 showMeLeapYearsButton.addEventListener("click", () => {calculateLeapYears()})
-sqPhraseButton.addEventListener("click", () => {sqPhraseDiv.style.display = "block"})
-pigLatinButton.addEventListener("click", () => {pigLatinDiv.style.display = "block"})
 
 function calculateLeapYears () {
-	let leapYearInput = parseInt(document.getElementById("leapYearInput").value)
-	let leapYearArray = []
+	let leapYearInput = parseInt(document.getElementById("leapYearInput").value);
+	let leapYearArray = [];
+	nextTwentyLeapYears.innerHTML = "";
 
 	do {
 		if (leapYearInput % 400 == 0 || (leapYearInput % 4 == 0 && !(leapYearInput % 100 == 0))) {
@@ -27,5 +24,42 @@ function calculateLeapYears () {
 	}
 	while (leapYearArray.length <= 19);
 
-	console.log("leap years: ", leapYearArray);
+	for (let i = 0; i < leapYearArray.length; i++) {
+		nextTwentyLeapYears.innerHTML += `<div>${leapYearArray[i]}</div>`
+	};
 }
+
+
+/****************
+SQUARE PHRASE
+****************/
+
+const sqPhraseDiv = document.getElementById("sqPhrase");
+const sqPhraseButton = document.getElementById("sqPhraseButton");
+const printSqPhraseButton = document.getElementById("printSqPhraseButton");
+sqPhraseButton.addEventListener("click", () => {sqPhraseDiv.style.display = "block"});
+printSqPhraseButton.addEventListener("click", () => {printSqPhrase()});
+
+function printSqPhrase () {
+	let phrase = document.getElementById("sqPhraseInput").value;
+	let phraseArray = phrase.split(" ");
+	let width = phraseArray.reduce((x, y) => {return x.length > y.length ? x : y}).length;
+
+	console.log(("*").repeat(width + 4));
+	for (let i = 0; i < phraseArray.length; i++) {
+		if (phraseArray[i].length < width) {
+			let spaces = width - phraseArray[i].length
+			console.log("* " + phraseArray[i] + (" ").repeat(spaces) + " *");
+		} else {
+			console.log(`* ${phraseArray[i]} *`);
+		}
+	}
+	console.log(("*").repeat(width + 4));
+}
+
+/****************
+PIG LATIN
+****************/
+const pigLatinButton = document.getElementById("pigLatinButton")
+const pigLatinDiv = document.getElementById("pigLatin")
+pigLatinButton.addEventListener("click", () => {pigLatinDiv.style.display = "block"})
--- tech-int/src/tech_int/core.clj
+++ tech-int/src/tech_int/core.clj
@@ -1,10 +1,59 @@
 (ns tech-int.core
   (:gen-class))
 
-(defn -leapYear
-  []
-  (println "Enter a year: ")
-  (def inputYear (Integer. (read-line)))
-  (println (str "You entered " inputYear "!"))
-  (println (type inputYear))
+; LEAP YEAR
+
+; (and (zero? (mod year 400)) (and (zero? (mod year 4)) (not= (zero? (mod year 100)))))
+
+; (zero? (mod year 400))
+
+; (and (zero? (mod year 4)) (not= (zero? (mod year 100))))
+
+
+
+(defn checkIfLeapYear
+    [year]
+    (println year)
+    (if (or (zero? (mod year 400)) (and (zero? (mod year 4)) (not= 0 (mod year 100))))
+        true
+        false
+    )
 )
+
+; (defn -leapYear []
+;     (println "Enter a year: ")
+;     (def inputYear (Integer. (read-line)))
+;     (loop [leapYearVector []
+;            year inputYear]
+;         (when (< (count leapYearVector) 20)
+;             (if (checkIfLeapYear year)
+;                 (do
+;                     (conj leapYearVector year)
+;                     (println year)
+;                 ))
+
+;             (recur leapYearVector (inc year))
+;         )
+;     )
+; )
+
+(defn leapYear [year]
+    (def i (atom 1))
+    (while (<= @i 20)
+        (do
+            (if (checkIfLeapYear year)
+                (do
+                    (println year)
+                    (inc year)
+                    (swap! @i inc)
+                )
+                (inc year)
+            )
+        )
+    )
+)
+
+
+(println "Enter a year: ")
+(def inputYear (Integer. (read-line)))
+(leapYear inputYear)
