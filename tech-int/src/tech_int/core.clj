b7c743c5efca1c90627132a02f18a12247e932e9
Joseph Kane <josephckane@gmail.com>
2016-11-10 11:16:38 -0600

    pig latin to english clojure, need to fix pig latin validator

--- tech-int/src/tech_int/core.clj
+++ tech-int/src/tech_int/core.clj
@@ -33,20 +33,69 @@
 
 ;;; PIG LATIN ;;;
 
-(use '[clojure.string :only (join split capitalize)])
+(use '[clojure.string :only (join split capitalize ends-with?)])
 
 (defn toPigLatin
     [string]
     (str (join (rest string)) (first string) (str "ay"))
 )
 
+(defn checkIfPigLatin
+    [array]
+    (loop [array array
+           i 0]
+        (if (< i (count array))
+            (if (not= (ends-with? (array i) "ay"))
+                false
+                true
+            )
+            (recur array (inc i))
+        )
+    )
+
+)
+
+(defn toEnglish
+    [string]
+    (def noAy (drop-last 2 string))
+    (def restOfWord (drop-last noAy))
+    (def firstLetter (last noAy))
+    (apply str firstLetter restOfWord)
+)
+
 (defn -pigLatin []
+    (println "1. English to pig latin")
+    (println "2. Pig latin to english")
+    (def choice (Integer. (read-line)))
     (println "Enter a phrase:")
     (def inputPhrase (read-line))
     (println "--")
     (def inputArray (split inputPhrase #"\s+"))
-    (def pigLatinArray (mapv toPigLatin inputArray))
-    (println (capitalize (join " " pigLatinArray)))
+    (if (< choice 3)
+        (if (= choice 1)
+            (do
+                (def pigLatinArray (mapv toPigLatin inputArray))
+                (println (capitalize (join " " pigLatinArray)))
+            )
+            (do
+                (if (checkIfPigLatin inputArray)
+                    (do
+                        (def englishArray (mapv toEnglish inputArray))
+                        (println (capitalize (join " " englishArray)))
+                    )
+                    (do
+                        (println "Phrase is not pig latin, please correct and try again.")
+                        (-pigLatin)
+                    )
+                )
+            )
+        )
+        (do
+            (println "Sorry, invalid choice")
+            (-pigLatin)
+        )
+    )
+
     (println "--")
 )
 
